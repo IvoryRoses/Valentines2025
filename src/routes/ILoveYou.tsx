be97640271594570a27707ec948c965d313fc6fc
IvoryRoses <sebastianabuyo@gmail.com>
2025-02-10 07:52:56 +0800

    change bg color

--- src/routes/ILoveYou.tsx
+++ src/routes/ILoveYou.tsx
@@ -3,7 +3,7 @@ import { FaHeart } from "react-icons/fa";
 
 export default function ILoveYou() {
   return (
-    <div className="font-sour flex h-screen flex-col items-center justify-center overflow-hidden bg-[#fce4f2]">
+    <div className="font-sour flex h-screen flex-col items-center justify-center overflow-hidden bg-[#ce9eba]">
       <div className="font-sour flex gap-3 text-3xl">
         <h1>YAYAYAYYAYAYY I Love You!</h1>
         <div>
